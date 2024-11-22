4db8ae0a3613207da0ced503412b2220200187c8
Pranavprj <jadhavpranav64@gmail.com>
2024-11-22 09:45:57 +0530

    checkout

--- MainProject/order/urls.py
+++ MainProject/order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/',views.checkout,name='checkout'),
+    path('checkout/',views.Checkout.as_view(),name='checkout'),
+    path('checkout/',views.proceed_to_pay,name='checkout'),
 ]
--- MainProject/order/views.py
+++ MainProject/order/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from accounts.models import Address
+from django.views import View
+from accounts.models import Address
 
 
-@login_required
-def checkout(request):
-    context = {
 
-    }
-    return render(request,'order/checkout.html',context)
+# @login_required(login_url='/accounts/login/')
+class Checkout(View):
+    # @login_required
+    def get(self,request):
+        address = Address.objects.filter(user = request.user)
+        context={
+            'address' : address
+        }
+        return render(request,'order/checkout.html',context)
+    # @login_required
+    def post(self,request):
+        address_id = request.POST.get('address')
+        if address_id:
+            request.session['address_id'] = address_id
+            return render(request, 'order/proceed_to_pay.html')
+            # return redirect('proceed_to_pay')
+        else:
+            return render(request, 'order/proceed_to_pay.html')
+        
+
+def proceed_to_pay(request):
+    return redirect(request,'order/proceed_to_pay')
